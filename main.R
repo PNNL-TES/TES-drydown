5ef58230202dea99cafeee37f637b0b7e8064741
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2018-12-04 07:02:11 -0500

    Outline future data pipeline

--- main.R
+++ main.R
@@ -1,5 +1,5 @@
 # Main project file
-# November 13, 2018 BBL
+# December 4, 2018 BBL
 
 library(drake)  # 6.1.0
 pkgconfig::set_config("drake::strings_in_dots" = "literals")
@@ -18,11 +18,26 @@ do_filedigest <- function(dir) digest::digest(list.files(dir)) # helper function
 
 plan <- drake_plan(
   
+  # Sample information
+  # core_data = ...
+  
   # Picarro data from drydown cores
   # We digest the filename list to detect when something changes in the licor_data directory
   raw_licor_data = target(command = read_picarro_dir("data/picarro/"),
                           trigger = trigger(change = do_filedigest("data/picarro/"))),
 
+  # Valve map file linking Picarro `MPVPosition` field to cores
+  # valve_map = ...
+  
+  # Compute per-sample change rate (ppmCO2/s and ppbCH4/s)
+  # summary_data = ...
+  
+  # Link with core data via valvemap
+  # summary_data_cores = ...
+  
+  # Compute fluxes
+  # flux_data = ...
+  
   # --------------------------------------------------------------------------------------------------------
   # Webpage diagnostics report
   diagnostics_report = rmarkdown::render(
