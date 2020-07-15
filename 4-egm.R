ab91bdeb8fb4998824da227cc290a3d02943fd86
Kaizad <kaizad.patel@pnnl.gov>
2020-07-15 09:45:14 -0700

    add egm-4 data and processing script

---
+++ 4-egm.R
@@ -0,0 +1,14 @@
+
+source("0-drydown_functions.R")
+
+egm_data = read.csv("data/respiration_egm_jul2020.csv")
+
+egm_data_cleaned = 
+  egm_data %>% 
+  mutate(date = dmy(date)) %>% 
+  select(date, Site, Core, PPM_CO2, Mass_g)
+
+
+# output ------------------------------------------------------------------
+
+write.csv(egm_data_cleaned, "data/processed/egm_concentrations.csv", row.names = F)
