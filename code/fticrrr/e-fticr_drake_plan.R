d01a0125ca476f71d9a20917f45d6f3916ec75c7
kaizadp <50244730+kaizadp@users.noreply.github.com>
2021-01-30 16:46:39 -0800

    add tzero report, VK, PCA, PERM, relabund.

--- code/fticrrr/e-fticr_drake_plan.R
+++ code/fticrrr/e-fticr_drake_plan.R
@@ -15,7 +15,8 @@
 
 # 1. SET input file paths -------------------------------
 COREKEY = "data/processed/corekey.csv"
-REPORT = "data/fticr/TES_drought_soil_Report.csv"
+REPORT1 = "data/fticr/TES_drought_soil_Report1_2020-11-05.csv"
+REPORT2 = "data/fticr/TES_drought_soil_Report2_timezero_2021-01-20.csv"
 DOCKEY = "data/doc_analysis_key.csv"
 
 ## SET the treatment variables
@@ -33,7 +34,10 @@ source("code/fticrrr/d-functions_statistics.R")
 # 3. load drake plans -----------------------------------------------------
 fticr_processing_plan = drake_plan(
   # a. PROCESSING ---- 
-  datareport = read.csv(file_in(REPORT)),
+  report1 = read.csv(file_in(REPORT1)),
+  report2 = read.csv(file_in(REPORT2)),
+  
+  datareport = combine_fticr_reports(report1, report2),
   fticr_meta = make_fticr_meta(datareport)$meta2,
   fticr_data_longform = make_fticr_data(datareport, depth, Site, length, drying, saturation)$data_long_key_repfiltered,
   fticr_data_trt = make_fticr_data(datareport, depth, Site, length, drying, saturation)$data_long_trt,
@@ -42,9 +46,7 @@ fticr_processing_plan = drake_plan(
   relabund_cores = fticr_data_longform %>% 
     compute_relabund_cores(fticr_meta, depth, Site, length, drying, saturation),
   
-  gg_relabund_bar = relabund_cores %>% plot_relabund(TREATMENTS)+
-    scale_fill_manual(values = PNWColors::pnw_palette("Sailboat"))+
-    facet_grid(Site+depth~drying+saturation),
+  gg_relabund_bar = relabund_cores %>% plot_relabund(TREATMENTS),
   
   ## create relabund table
   
@@ -53,13 +55,16 @@ fticr_processing_plan = drake_plan(
   gg_vankrevelens = plot_vankrevelens(fticr_data_trt, fticr_meta),
   gg_vk_newpeaks_saturation = plot_vk_saturation(fticr_data_trt, fticr_meta),
   gg_vk_newpeaks_drying = plot_vk_drying(fticr_data_trt, fticr_meta),
+  gg_vk_timezero = plot_vk_timezero(fticr_data_trt, fticr_meta),
   
   # d. STATISTICS ---- 
   ## PERMANOVA
   fticr_permanova = compute_permanova(relabund_cores),
+  fticr_permanova_tzero = compute_permanova_tzero(relabund_cores),
   
   ## PCA
   gg_pca = compute_fticr_pca(relabund_cores), 
+  gg_pca_tzero = compute_fticr_pca_tzero(relabund_cores),
   
   # e. OUTPUT FILES ----
   #  fticr_meta %>% write.csv(),
