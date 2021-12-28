55eada569b71030066af80ba11827f9b83713135
kaizadp <50244730+kaizadp@users.noreply.github.com>
2021-12-28 11:24:59 -0800

    script for tctn/pom data

--- code/5-tctn_processing.R
+++ code/5-tctn_processing.R
@@ -1,5 +1,9 @@
 source("code/0-drydown_functions.R")
 
+
+#
+# -------------------------------------------------------------------------
+
 tctn_data = read.csv("data/combined_site_tctn.csv", na.strings = "")
 corekey = read.csv("data/processed/corekey.csv")
 tctn_processed = 
@@ -64,3 +68,91 @@ pom_data = read.csv("data/combined_site_pom.csv")
 pom_key = read.csv("data/pom_key.csv")
 corekey = read.csv("data/processed/corekey.csv")
 
+
+
+# -------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+
+filePaths_tctn <- list.files(path = "data/TCTN",pattern = "*.csv", full.names = TRUE)
+
+tctn_data <-
+  lapply(filePaths_tctn, read.delim, sep = ",",  na.string = "") %>% 
+  bind_rows() %>% 
+  dplyr::select(Info, Name, `N.....`, `C.....`, Memo, `Date.......Time`) %>% 
+  rename(TN_perc = `N.....`,
+         TC_perc = `C.....`,
+         datetime = `Date.......Time`)
+
+tctn_data_processed = 
+  tctn_data %>% 
+  filter(grepl("S[0-9]", Name) | grepl("C[0-9]", Name) | grepl("POM", Name)) %>% 
+  filter(is.na(Memo)) %>% 
+  mutate(analysis = if_else(grepl("POM", Name), "POM-nonPOM", "totalCN"))
+
+
+totalcn_data = 
+  tctn_data_processed %>% 
+  filter(analysis == "totalCN")
+  
+pom_data = 
+  tctn_data_processed %>% 
+  filter(analysis == "POM-nonPOM") %>% 
+  mutate(Name = str_remove(Name, " ")) %>% 
+  left_join(pom_key, by = c("Name" = "POM_ID"))
+
+
+pom_data_temp1 = 
+  pom_data %>% 
+  filter(!is.na(coreID))
+
+pom_data_temp2 = 
+  pom_data %>% 
+  filter(is.na(coreID)) %>% 
+  mutate(POM = case_when(grepl("NPOM", Name) ~ "non-POM",
+                         grepl("POM", Name) ~ "POM"),
+         Depth = case_when(grepl("0-5", Name) ~ "0-5 cm",
+                           grepl("5-end", Name) ~ "5cm-end"),
+         Name = str_remove(Name, "NPOM"),
+         Name = str_remove(Name, "POM"),
+         Name = str_remove(Name, "0-5"),
+         Name = str_remove(Name, "5-end"),
+         Name = str_remove(Name, " "),
+         Name = str_remove(Name, "-"),
+         coreID = Name)
+  
+pom_data_cleaned = 
+  bind_rows(pom_data_temp1, pom_data_temp2) %>% 
+  dplyr::select(Info, Name, coreID, Depth, POM, TC_perc, TN_perc) %>% 
+  left_join(corekey_full %>% dplyr::select(coreID, Site, drying, length, location, saturation))
+
+
+pom_key = read.csv("data/pom_key.csv")
+corekey = read.csv("data/processed/corekey.csv", na = "")
+
+
+
+pom_data_cleaned %>% 
+  ggplot(aes(x = length, y = TC_perc, color = POM))+
+  geom_point(position = position_dodge(width = 0.3))+
+  facet_grid(drying ~ Site + Depth)
+
+
+
+# clean corekey -----------------------------------------------------------
+dockey = read.csv("data/doc_analysis_key.csv", na = "")
+
+corekey_full = 
+  full_join(corekey, dockey) %>% 
+  mutate(location = recode(location, "up" = "upland", 
+                           low = "lowland"),
+         drying = recode(drying, "forced" = "FAD", 
+                         "constant weight" = "CW"),
+         length = str_replace(length, "day", "d"),
+         DOC_analysis = !is.na(DOC_ID)) %>% 
+  distinct(coreID, Site, location, drying, length, saturation, DOC_analysis) %>% 
+  group_by(coreID) %>% 
+  mutate(n = n()) %>% 
+  filter(n == 1 | (n == 2 & DOC_analysis == TRUE)) %>% 
+  dplyr::select(-n) %>% 
+  mutate(saturation = if_else(DOC_analysis == FALSE, "saturated", saturation))
+  
