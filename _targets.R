be2deed20eebb7033dfc2b15aa244c7b99fd0793
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2025-06-30 15:39:10 -0700

    update filepaths

--- _targets.R
+++ _targets.R
@@ -16,28 +16,28 @@ tar_option_set(
 
 
 # Run the R scripts in the R/ folder with your custom functions:
-source("code/0-packages.R")
-source("code/1-functions_processing.R")
-#source("code/2-functions_analysis.R")
-source("code/1b-fticrrr_processing.R")
+source("2-code/0-packages.R")
+source("2-code/1-functions_processing.R")
+#source("2-code/2-functions_analysis.R")
+source("2-code/1b-fticrrr_processing.R")
 
 
 # Replace the target list below with your own:
 list(
-  tar_target(sample_key_data, "data/sample_key.csv", format = "file"),
+  tar_target(sample_key_data, "1-data/sample_key.csv", format = "file"),
   tar_target(sample_key, read.csv(sample_key_data, na = "")),
-  tar_target(doc_key_data, "data/doc_analysis_key.csv", format = "file"),
+  tar_target(doc_key_data, "1-data/doc_analysis_key.csv", format = "file"),
   tar_target(doc_key, read.csv(doc_key_data, na = "")),
-  tar_target(subsampling_data, "data/subsampling.csv", format = "file"),
+  tar_target(subsampling_data, "1-data/subsampling.csv", format = "file"),
   tar_target(subsampling, read.csv(subsampling_data, na = "")),
   tar_target(dry_weights, compute_dry_weights(subsampling)),
   
   ##        # weoc
-  ##      #  tar_target(weoc_data, import_weoc_data(FILEPATH = "data/npoc")),
+  ##      #  tar_target(weoc_data, import_weoc_data(FILEPATH = "1-data/npoc")),
   ##      #  tar_target(weoc_processed, process_weoc(weoc_data, doc_key, dry_weights)),
   ##        
   ##        # fticr
-  tar_target(fticr_report, import_fticr(FILEPATH = "data/fticr")),
+  tar_target(fticr_report, import_fticr(FILEPATH = "1-data/fticr")),
   tar_target(fticr_meta, process_fticr(fticr_report, doc_key, sample_key)$fticr_meta),
   tar_target(fticr_long, process_fticr(fticr_report, doc_key, sample_key)$data_long),
   tar_target(fticr_trt, process_fticr(fticr_report, doc_key, sample_key)$data_long_trt),
@@ -46,13 +46,13 @@ list(
   ##        
   ##        # NMR
   ##        ## spectra
-  tar_target(nmr_spectra, nmr_import_spectra("data/nmr-data/nmr_spectra", method = "mnova")),
+  tar_target(nmr_spectra, nmr_import_spectra("1-data/nmr-data/nmr_spectra", method = "mnova")),
   tar_target(nmr_spectra_processed, process_nmr_spectra(nmr_spectra, doc_key, sample_key)),
   ##      #  tar_target(gg_nmr_spectra_all, plot_nmr_spectra_all(nmr_spectra_processed)),
   ##      #  tar_target(gg_nmr_spectra_subset, plot_nmr_spectra_subset(nmr_spectra_processed)),
   ##        
   ##        ## peaks
-  tar_target(nmr_peaks, nmr_import_peaks("data/nmr-data/nmr_peaks", method = "multiple columns")),
+  tar_target(nmr_peaks, nmr_import_peaks("1-data/nmr-data/nmr_peaks", method = "multiple columns")),
   tar_target(nmr_peaks_processed, process_nmr_peaks(nmr_peaks, doc_key, sample_key)),
   tar_target(nmr_relabundance, compute_nmr_relabund(nmr_peaks_processed, doc_key, sample_key)),
   
@@ -67,9 +67,9 @@ list(
   ##        
   ##        # export
   ##        tar_target(export, {
-  ##          write.csv(fticr_meta, "data/processed/fticr_meta.csv", row.names = FALSE)
-  ##          crunch::write.csv.gz(fticr_long, "data/processed/fticr_long_all_samples.csv.gz", row.names = FALSE)
-  ##          crunch::write.csv.gz(fticr_trt, "data/processed/fticr_long_treatments.csv.gz", row.names = FALSE)
+  ##          write.csv(fticr_meta, "1-data/processed/fticr_meta.csv", row.names = FALSE)
+  ##          crunch::write.csv.gz(fticr_long, "1-data/processed/fticr_long_all_samples.csv.gz", row.names = FALSE)
+  ##          crunch::write.csv.gz(fticr_trt, "1-data/processed/fticr_long_treatments.csv.gz", row.names = FALSE)
   ##          
   ##        }, format = "file")
   
